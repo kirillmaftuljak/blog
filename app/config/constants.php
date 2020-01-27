48cfd8fc65178ef56850113fb87dc918e28a676c
Kirill Maftulyak <kirillmaftulyak@Kirills-MacBook-Pro.local>
2020-01-27 14:46:45 +0200

    add framework files

---
+++ app/config/constants.php
@@ -0,0 +1,16 @@
+<?php
+
+
+define('PROJECT_NAME', 'pvk');
+
+define('URLROOT', 'http://'.$_SERVER['HTTP_HOST'].'/'.PROJECT_NAME);
+
+//rakenduse juurkataloog
+define('APPROOT', dirname(dirname(__FILE__)));
+
+// db parameetrid
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'kirill');
+define('DB_BAAS', '');
+define('DB_NAME', 'dbo');
---
+++ public/index.php
@@ -0,0 +1,5 @@
+<?php
+error_reporting(0);
+require_once '../app/bootstrap.php';
+// loome Core objekti raamatukogu kasutamiseks
+$init = new Core();
